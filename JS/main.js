1a29fdd9842220d00c7ab221a55f17e44a0359f0
melissamclaughlin <melissamclaughlindesigns@gmail.com>
2016-03-17 09:23:20 -0400

    Created Repo

---
+++ JS/main.js
@@ -0,0 +1,35 @@
+ $(document).ready(function() {
+  $('a[href*=#]').each(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname
+    && this.hash.replace(/#/,'') ) {
+      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+       if ($target) {
+         var targetOffset = $target.offset().top;
+
+   
+         $(this).click(function() {
+            $("#nav li a").removeClass("active");
+            $(this).addClass('active');
+           $('html, body').animate({scrollTop: targetOffset}, 1000);
+           return false;
+         });
+      }
+    }
+  });
+
+});
+
+
+$(document).ready(function(){
+ 
+    $( ".oldenough" ).click(function() {
+                $( ".popup" ).fadeOut( 1200 );
+  });
+ 
+  $( ".notoldenough" ).click(function() {
+                window.open("http://www.google.com","_self");
+  });
+ 
+});
